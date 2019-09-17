d245425dfd08801d0a0988688ad62000da84a055
sbx_user1051 <sbx_user1051@169.254.1.33>
2019-09-17 08:43:17 +0000

    Tue Sep 17 08:43:17 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y:int) -> int:
     if x < 0 or x > 100 or y < 0 or y > 100:
-        error_str = "The values specified for sum should be between 0 and 100"
+        error_str = "The parameters for sum_solution should be between 0 and 100"
         error_str += " but are {0}, {1}".format(x, y)
         raise ValueError(error_str)
     return x + y
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,9 +6,20 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
-    def test_para_boundaries(self):
+    def test_parameter_less_than_zero(self):
         with pytest.raises(ValueError):
-            assert sum_solution(-1, 1) 
+            assert sum_solution.compute(-1, 1) 
         with pytest.raises(ValueError):
-            assert sum_solution(-1, -1) 
+            assert sum_solution.compute(-1, -1) 
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(1, -1) 
+
+    def test_parameter_greater_than_one_hundred(self):
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(101, 1) 
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(101, 101) 
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(1, 101) 
+
 
