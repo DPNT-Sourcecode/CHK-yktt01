efbd52c9ceaddbf83952e5364416a8bb332ef2cc
sbx_user1051 <sbx_user1051@169.254.59.173>
2019-09-17 10:15:49 +0000

    Tue Sep 17 10:15:49 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,14 +1,26 @@
+import random
+import copy
 from checkout import checkout
-from random import shuffle
 
 
 def test_no_offers():
     prices = {'A': 50, 'B': 60, 'C': 70}
     p_c = PriceCalculator(prices, {})
-    assert checkout('AAAAABBBBBBBBBBCCCCCCCCCCCCCCCCCCCC')== 300 + 600 + 3500
+    items = 'AAAAABBBBBBBBBBCCCCCCCCCCCCCCCCCCCC'
+    assert checkout(items) == 300 + 600 + 3500
+    #Lets shuffle to make sure that works
+    items_shuffled = copy.copy(items_str)
+    random.seed(7)
+    random.shuffle(items_shuffled)
+    assert checkout(items_shuffled) == 300 + 600 + 3500
+
 
 def test_offers():
     prices = {'A': 50, 'B': 60, 'C': 70}
     offers = {'A': (2, 75), 'B': (3, 150)}
     p_c = PriceCalculator(prices, offers)
-    assert checkout('AAAAABBBBBBBBBBCCCCCCCCCCCCCCCCCCCC')) == 225 + 510 + 3500
+    items = 'AAAAABBBBBBBBBBCCCCCCCCCCCCCCCCCCCC'
+    assert checkout(shuffled) == 225 + 510 + 3500
+    #Lets shuffle to make sure that works
+    items_shuffled = copy.copy(items_str)
+
