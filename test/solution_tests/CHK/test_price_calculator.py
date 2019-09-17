a64d436c893eb99d1480365058cdf32f7e75330d
sbx_user1051 <sbx_user1051@169.254.59.173>
2019-09-17 10:00:49 +0000

    Tue Sep 17 10:00:49 UTC 2019

--- test/solution_tests/CHK/test_price_calculator.py
+++ test/solution_tests/CHK/test_price_calculator.py
@@ -3,4 +3,6 @@ from checkout import PriceCalculator
 def test_no_offers():
     prices = {'A': 50, 'B': 60, 'C': 70}
     p_c = PriceCalculator(prices, {})
-    assert p_c.calc({'A': 6, 'B': 10, 'C': 20})
+    assert p_c.calc({'A': 6, 'B': 10, 'C': 20}) == 300 + 600 + 3500
+
+def test_offers():
