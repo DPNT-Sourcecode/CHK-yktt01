e859d0c85eb27af8341d8ab8deeac9f1bb0f751a
sbx_user1051 <sbx_user1051@169.254.135.21>
2019-09-17 09:16:18 +0000

    Tue Sep 17 09:16:18 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,9 @@
+import typing as t
 
+def checkout_list(skus: t.List[str])
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus_str: str):
     raise NotImplementedError()
+
