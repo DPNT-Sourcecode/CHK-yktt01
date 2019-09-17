550244056899c0aa8671e842b44feca3d5ef6173
sbx_user1051 <sbx_user1051@169.254.1.33>
2019-09-17 08:40:17 +0000

    Tue Sep 17 08:40:17 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,14 @@
 from solutions.SUM import sum_solution
+import pytest
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_para_boundaries(self):
+        with pytest.raises(ValueError):
+            assert sum_solution(-1, 1) 
+        with pytest.raises(ValueError):
+            assert sum_solution(-1, -1) 
+
