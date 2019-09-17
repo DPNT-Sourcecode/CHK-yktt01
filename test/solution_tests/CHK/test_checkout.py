00b557f5d7c66ca13d439b0b1e031561910f2015
sbx_user1051 <sbx_user1051@169.254.187.241>
2019-09-17 12:07:37 +0000

    Tue Sep 17 12:07:37 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -10,29 +10,22 @@ def shuffle_str(s: str) -> str:
     return str.join('', li)
 
 
-def test_no_offers():
-    prices = {'A': 50, 'B': 60, 'C': 70}
-    items = 'A'*6 + 'B'*10 + 'C' * 20
-    assert checkout(items, prices=prices, offers={}) == 300 + 600 + 1400
-    #Lets shuffle to make sure that works
-    items_shuffled = shuffle_str(items)
-    assert checkout(
-        items_shuffled, prices=prices, offers={},
-    ) == 300 + 600 + 1400
+OFFERS = {
+    'A': [(2, 75), (1, 50)],
+    'B': [(3, 150), (1, 60)],
+    'C': [(1, 70)]
+}
 
 
 def test_offers():
-    prices = {'A': 50, 'B': 60, 'C': 70}
-    offers = {'A': (2, 75), 'B': (3, 150)}
     items = 'A'*6 + 'B'*10 + 'C' * 20
-    assert checkout(items, prices=prices, offers=offers) == 225 + 510 + 1400
+    assert checkout(items, offers=OFFERS) == 225 + 510 + 1400
     #Lets shuffle to make sure that works
     items_shuffled = shuffle_str(items)
-    assert checkout(
-        items_shuffled, prices=prices, offers=offers,
-    ) == 225 + 510 + 1400
+    assert checkout(items_shuffled, offers=OFFERS) == 225 + 510 + 1400
+
 
 def test_price_not_found():
     prices = {'A': 50, 'B': 60, 'C': 70}
     items = 'A'*6 + 'B'*10 + 'C' * 20 + 'D'
-    assert checkout(items, prices=prices, offers={}) == -1
+    assert checkout(items, offers=OFFERS) == -1
--- test/solution_tests/CHK/test_price_calculator.py
+++ test/solution_tests/CHK/test_price_calculator.py
@@ -1,21 +1,21 @@
 from solutions.CHK.checkout_solution import PriceCalculator, PriceNotFoundError
 import pytest
 
+OFFERS = {
+    'A': [(2, 75), (1, 50)],
+    'B': [(3, 150), (1, 60)],
+    'C': [(1, 70)]
+}
 
 def test_offers():
-    prices = {'A': 50, 'B': 60, 'C': 70}
-    offers = {
-        'A': [(2, 75), (1, 50)],
-        'B': [(3, 150), (1, 60)],
-        'C': [(1, 70)]}
-    p_c = PriceCalculator(prices, offers)
+    p_c = PriceCalculator(OFFERS)
     assert p_c.calc('A', 6) == 225 
     assert p_c.calc('B', 10) == 510
     assert p_c.calc('C', 20) == 1400
 
 def test_price_not_found():
-    prices = {'A': 50, 'B': 60, 'C': 70}
-    p_c = PriceCalculator(prices, {})
+    p_c = PriceCalculator(OFFERS)
     with pytest.raises(PriceNotFoundError):
         assert p_c.calc('D', 6)
+
 
