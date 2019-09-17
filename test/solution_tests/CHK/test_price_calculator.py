51a679120e9cc168fd94df8c45afe33cd408d55d
sbx_user1051 <sbx_user1051@169.254.59.173>
2019-09-17 10:03:49 +0000

    Tue Sep 17 10:03:49 UTC 2019

--- test/solution_tests/CHK/test_price_calculator.py
+++ test/solution_tests/CHK/test_price_calculator.py
@@ -6,3 +6,7 @@ def test_no_offers():
     assert p_c.calc({'A': 6, 'B': 10, 'C': 20}) == 300 + 600 + 3500
 
 def test_offers():
+    prices = {'A': 50, 'B': 60, 'C': 70}
+    offers = {'A': (2, 75), 'B': (3, 150)}
+    p_c = PriceCalculator(prices, offers)
+    assert p_c.calc({'A': 6, 'B': 10, 'C': 20}) == 225 + 510 + 3500
