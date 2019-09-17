b26b40a34e4275c98a39e08ff450769a0fbd5211
sbx_user1051 <sbx_user1051@169.254.59.173>
2019-09-17 10:33:49 +0000

    Tue Sep 17 10:33:49 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,16 +3,21 @@ import copy
 from solutions.CHK.checkout_solution import checkout
 
 
+def shuffle_str(s: str) -> str:
+    random.seed(7)
+    li = s.split('')
+    random.shuffle(li)
+    return str.join('', li)
+
+
 def test_no_offers():
     prices = {'A': 50, 'B': 60, 'C': 70}
     items = 'A'*6 + 'B'*10 + 'C' * 20
     assert checkout(items, prices=prices, offers={}) == 300 + 600 + 1400
     #Lets shuffle to make sure that works
-    items_shuffled = copy.copy(items)
-    random.seed(7)
-    random.shuffle(items_shuffled)
+    items_shuffled = shuffle_str(copy.copy(items))
     assert checkout(
-        items_shuffled, prices=prices, offers=offers
+        items_shuffled, prices=prices, offers={},
     ) == 300 + 600 + 1400
 
 
@@ -22,10 +27,11 @@ def test_offers():
     items = 'A'*6 + 'B'*10 + 'C' * 20
     assert checkout(items, prices=prices, offers=offers) == 225 + 510 + 1400
     #Lets shuffle to make sure that works
-    items_shuffled = copy.copy(items)
+    items_shuffled = shuffle_str(copy.copy(items))
     assert checkout(
-        items_shuffled, prices=prices, offers=offers
+        items_shuffled, prices=prices, offers=offers,
     ) == 225 + 510 + 1400
+
 
 
 
