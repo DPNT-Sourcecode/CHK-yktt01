e2579f7f3e850fa6d5cd24cf52c6b355a3a44ded
sbx_user1051 <sbx_user1051@169.254.135.21>
2019-09-17 09:19:18 +0000

    Tue Sep 17 09:19:18 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,21 @@
 import typing as t
 
-def checkout_list(skus: t.List[str])
+prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
+
+#Tabular price table only allows one offer per item
+offers = {
+    'A': (3, 130),
+    'B': (2, 45),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus_str: str):
+def checkout(skus: str):
     raise NotImplementedError()
+
 
