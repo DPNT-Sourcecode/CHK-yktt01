8030f9f8375e886430e8fa9db4b87be1aa4a2350
sbx_user1051 <sbx_user1051@169.254.194.173>
2019-09-20 11:05:37 +0000

    Fri Sep 20 11:05:37 UTC 2019

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -32,13 +32,30 @@ def test_price_not_found():
     assert checkout(items, offers=OFFERS) == -1
 
 
-BUY_OFFERS = (
-    (
-        BuyOfferSpec(item='A', number_required=3),
-        BuyOfferDiscount(item="B", number_to_discount=1),
+def test_applying_buy_offer_is_cheaper():
+    buy_offers = (
+        (
+            BuyOfferSpec(item='A', number_required=3),
+            BuyOfferDiscount(item="B", number_to_discount=1),
+        )
     )
-)
+    items = 'A'*6 + 'B'*10 + 'C' * 20
+    assert checkout(items, offers=OFFERS, buy_offers=buy_offers) == 225 + 480 + 1400
+    #Lets shuffle to make sure that works
+    items_shuffled = shuffle_str(items)
+    assert checkout(items_shuffled, offers=OFFERS, buy_offers=buy_offers) == 225 + 480 + 1400
 
-def test_buy_offer():
-    items = 'A'*6 + 'B'*10 + 'C' * 20 + 'D'
-    assert checkout(items, offers=OFFERS) == -1
+
+def test_applying_buy_offer_is_not_cheaper():
+    buy_offers = (
+        (
+            BuyOfferSpec(item='A', number_required=6),
+            BuyOfferDiscount(item="B", number_to_discount=1),
+        )
+    )
+    items = 'A'*6 + 'B'*10 + 'C' * 20
+    assert checkout(items, offers=OFFERS, buy_offers=buy_offers) == 225 + 510 + 1400
+    #Lets shuffle to make sure that works
+    items_shuffled = shuffle_str(items)
+    assert checkout(items_shuffled, offers=OFFERS, buy_offers=buy_offers) == 225 + 510 + 1400
+
